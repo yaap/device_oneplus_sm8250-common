4e557ac13f5bceabf3fc9fb53bdd0d135e133d38
Adhitya Mohan <adhitya@yaosp.dev>
2021-09-09 16:57:25 +0200

    sm8250-common: Update fp from coral September
    switch to coral in order to match https://github.com/yaap/frameworks_base/commit/ab8ad2cca79c5e596e41971d8d931ba800a8ece4

--- init/init_sm8250.cpp
+++ init/init_sm8250.cpp
@@ -225,5 +225,6 @@ void vendor_load_properties() {
   load_dalvikvm_properties();
 
   // fingerprint
-  property_override_multi("ro.build.fingerprint", "ro.vendor.build.fingerprint","ro.bootimage.build.fingerprint", "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys");
+    property_override("ro.build.description", "coral-user 11 RQ3A.210905.001 7511028 release-keys");
+    property_override_multi("ro.build.fingerprint", "ro.vendor.build.fingerprint","ro.bootimage.build.fingerprint", "google/coral/coral:11/RQ3A.210905.001/7511028:user/release-keys");
 }
